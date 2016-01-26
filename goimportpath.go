b69bbf3263f8f1b37c95cbdf924f61353d970368
Yi Wang <yi.wang.2005@gmail.com>
2016-01-26 11:53:09 -0800

    Add goimportpath.go

---
+++ goimportpath.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+const (
+	goTagsTemplate = `<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="go-import" content="{{.ImportPath}} git {{.RepoUrl}}">
+</head>
+<body>
+</body>
+</html>
+`
+)
+
+type Canonical struct {
+	ImportPath string
+	RepoUrl    string
+}
+
+var (
+	temp *template.Template
+
+	canonicals = map[string]Canonical{ // URL.Path ==> canonical
+		"/weakand": {
+			ImportPath: "topic.ai/weakand",
+			RepoUrl:    "github.com/wangkuiyi/weakand"},
+	}
+)
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+
+	if canonical, ok := canonicals[r.URL.Path]; ok {
+		temp.Execute(w, canonical)
+	} else {
+		fmt.Fprintf(w, "Hello! Welcome to %s!", r.URL.Host)
+	}
+}
+
+func main() {
+	addr := flag.String("addr", ":443", "Listening address")
+	flag.Parse()
+
+	if t, e := template.New("gotags").Parse(goTagsTemplate); e != nil {
+		log.Fatal(e)
+	} else {
+		temp = t
+	}
+
+	http.HandleFunc("/", viewHandler)
+	http.ListenAndServeTLS(*addr, "server.pem", "server.key", nil)
+}
